fdc33ef2821d795c8a6d58852ce941d3d3b4a6bf
Thomas007G <thomas007galligan@gmail.com>
2018-11-10 15:19:49 +0000

    added rest api

--- backend/main.go
+++ backend/main.go
@@ -6,23 +6,39 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/*
+type User struct {
+	username string
+	token    string
+}
+type UserAuth struct {
+	username string
+	password string
+}
+*/
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	r.GET("/test/:user/:repo/", func(c *gin.Context) {
+		user := c.Param("user")
+		repo := c.Param("repo")
+		//value, ok := db[user]
+		c.JSON(http.StatusOK, gin.H{"user": user, "repo": repo})
+	})
+
+	return r
+}
+
 func main() {
-	router := gin.Default()
-
-	// This handler will match /user/john but will not match /user/ or /user
-	router.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s", name)
-	})
-
-	// However, this one will match /user/john/ and also /user/john/send
-	// If no other routers match /user/john, it will redirect to /user/john/
-	router.GET("/user/:name/*action", func(c *gin.Context) {
-		name := c.Param("name")
-		action := c.Param("action")
-		message := name + " is " + action
-		c.String(http.StatusOK, message)
-	})
-
-	router.Run(":8080")
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
 }
