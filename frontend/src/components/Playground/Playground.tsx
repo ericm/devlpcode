4d3d6fbc25aee312bffcfb73cbb42c70775aeef1
TooFievFive <e.moynihan00@gmail.com>
2018-11-24 18:27:44 +0000

    aceditor

--- frontend/src/components/Playground/Playground.tsx
+++ frontend/src/components/Playground/Playground.tsx
@@ -1,9 +1,11 @@
+import * as brace from 'brace';
 import * as React from 'react';
+import AceEditor from 'react-ace';
 import { pgState } from '../../../interfaces';
 import Styles from './Playground.styles';
-
 const s = new Styles;
-
+brace.acequire('brace/theme/github');
+brace.acequire('brace/mode/javascript');
 class Playground extends React.Component<{}, pgState> {
 
 	constructor(props: any) {
@@ -11,10 +13,17 @@ class Playground extends React.Component<{}, pgState> {
 		this.state = {text: "console.log();"};
 		this.handler= this.handler.bind(this);
 	}
+
 	public render() {
 		return (
 			<article>
-				<pre id={"code"}><code>{this.state.text}</code></pre>
+				<AceEditor
+					mode="javscript"
+					theme="github"
+					name="coder"
+					fontSize={20}
+					editorProps={{$blockScrolling: true}}
+				/>
 				<textarea onKeyUp={this.handler} className={s.secret} />
 			</article>
 			
